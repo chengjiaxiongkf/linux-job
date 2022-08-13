82e7ad661fa9700de0c2067d1e9e122033460e5c
chengjiaxiongkf <514471552@qq.com>
2022-08-13 17:17:34 +0800

    增加fineBI的安装

---
+++ docker-compose/fineBI/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://fine-build.oss-cn-shanghai.aliyuncs.com/finebi/5.1.3/stable/exe/spider/linux_unix_FineBI5_1-CN.sh
+chmod 777 linux_unix_FineBI5_1-CN.sh
+./linux_unix_FineBI5_1-CN.sh
+
+echo "FineBI 官方文档 https://help.fanruan.com/finebi/doc-view-260.html"
