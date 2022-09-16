34fcd803ebbdfe9713816bce55b7aaf3de651361
jxcheng <514471552@qq.com>
2022-09-16 13:20:41 +0800

    install.sh

---
+++ docker-compose/cloudreve/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -vp ./volumes/cloudreve/{uploads,avatar} \
+&& touch ./volumes/cloudreve/conf.ini \
+&& touch ./volumes/cloudreve/cloudreve.db \
+&& mkdir -p ./volumes/cloudreve/aria2/config \
+&& mkdir -p ./volumes/cloudreve/data/aria2 \
+&& chmod -R 777 ./volumes/cloudreve/data/aria2
+docker-compose up -d 
