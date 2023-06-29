a63040636e202fd6f6cfc8365301e43835c18d3a
Mr.cheng <66402618+chengjiaxiongkf@users.noreply.github.com>
2023-06-29 11:13:00 +0800

    Delete install-docker-compose.sh

--- install-docker-compose.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose version
