b9cbb9f53165a01e77ccfeb7449904da06725041
514471552@qq.com <cjxwd86496048>
2023-07-25 16:53:19 +0800

    修改

--- install/install-docker.sh
+++ install/install-docker.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 sudo yum update sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 sudo yum install -y docker
