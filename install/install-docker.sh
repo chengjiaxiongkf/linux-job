8b887f937f6d9a9b45eab6aed044d24d83f7e59f
514471552@qq.com <cjxwd86496048>
2023-08-19 15:53:26 +0800

    feat:install-docker

--- install/install-docker.sh
+++ install/install-docker.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 sudo yum update sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-sudo yum install -y docker
+sudo yum install -y docker-ce docker-ce-cli
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+     "registry-mirrors": [
+         "http://hub-mirror.c.163.com",
+         "https://docker.mirrors.ustc.edu.cn",
+         "https://registry.docker-cn.com"
+     ]
+}
+EOF
 sudo systemctl enable docker
 sudo systemctl start docker
 docker version
