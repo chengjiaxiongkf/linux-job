800eacae9db24df039f9447337a2824e86c5a5fa
514471552@qq.com <cjxwd86496048>
2023-12-10 16:16:23 +0800

    frpc配置改版参数

---
+++ install/vpn.sh
@@ -0,0 +1,23 @@
+sudo yum update sudo yum install -y yum-utils device-mapper-persistent-data lvm2 wget
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+     "registry-mirrors": [
+         "http://hub-mirror.c.163.com",
+         "https://docker.mirrors.ustc.edu.cn",
+         "https://registry.docker-cn.com"
+     ]
+}
+EOF
+sudo systemctl enable docker
+sudo systemctl start docker
+docker version
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
+mkdir -p /data/shadowsocks
+cd /data/shadowsocks
+wget –no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+chmod +x shadowsocks-all.sh
+./shadowsocks-all.sh 2>&1| tee shadowsocks-all.log
