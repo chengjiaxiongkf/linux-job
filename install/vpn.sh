486ddbfc5056742aa59d1b03bb039966ec0a2b59
514471552@qq.com <cjxwd86496048>
2024-01-10 22:50:44 +0800

    vpn

--- install/vpn.sh
+++ install/vpn.sh
@@ -1,4 +1,4 @@
-sudo yum update sudo yum install -y yum-utils device-mapper-persistent-data lvm2 wget
+sudo yum update sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli
 sudo tee /etc/docker/daemon.json <<-'EOF'
@@ -18,6 +18,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
 mkdir -p /data/shadowsocks
 cd /data/shadowsocks
+yum install -y wget
 wget –no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
 ./shadowsocks-all.sh 2>&1| tee shadowsocks-all.log
